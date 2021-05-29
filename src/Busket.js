4a10908ae797a894854b0d79e50bfc83f41faefb
missixx <61702374+missixx@users.noreply.github.com>
2021-05-29 23:15:38 +0300

    added empty busket

--- src/Busket.js
+++ src/Busket.js
@@ -3,15 +3,27 @@ import BusketItem from './BusketItem'
 
 function Busket(props) {
 
-   const mapedItems = props.busketItems.map((item) => <BusketItem key={item.id} item={item}/>)
-   const totalAmount = props.busketItems.reduce((accumulator , current) => {
-      return +accumulator + (current.itemsCount * current.price); 
+   const mapedItems = props.busketItems.map((item) => <BusketItem key={item.id} item={item} />)
+   const totalAmount = props.busketItems.reduce((accumulator, current) => {
+      return +accumulator + (current.itemsCount * current.price);
    }, [])
-   
+
+   const emptyBusket = () => {
+      return (
+         <h2>BUSKET IS EMPTY...</h2>
+      )
+   }
+
+   const notEmptyBusket = () => {
+      return (
+         <h2>TOTAL AMOUNT: {totalAmount}</h2>
+      )
+   }
+
    return (
       <div>
          {mapedItems}
-         <h2>TOTAL AMOUNT: {totalAmount}</h2>
+         {props.busketItems.length === 0 ? emptyBusket() : notEmptyBusket()}
       </div>
    )
 }
